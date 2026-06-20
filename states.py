d34de0ec7dc0262a0d050339135375f38e169d36
Developer <dev@example.com>
2026-06-20 22:53:05 +0300

    feat(states): add gender, goal, looking_for and edit states

--- states.py
+++ states.py
@@ -7,5 +7,18 @@ class Registration(StatesGroup):
     policy = State()
     age = State()
     name = State()
+    gender = State()
+    looking_for = State()
+    goal = State()
+    interests = State()
+    photo = State()
+
+
+class EditProfile(StatesGroup):
+    choosing_field = State()
+    age = State()
+    name = State()
+    looking_for = State()
+    goal = State()
     interests = State()
     photo = State()
